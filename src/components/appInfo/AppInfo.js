a9e5a73f28c7df0896f1595f593f9b43562c86e2
jakubGora <jakub_gora@wp.pl>
2021-12-30 21:25:55 +0100

    Dodawanie opinii do szczegułów aplikacji oraz poprawa styli

--- src/components/appInfo/AppInfo.js
+++ src/components/appInfo/AppInfo.js
@@ -1,19 +1,40 @@
 import { useEffect, useState } from "react";
 import "./style/infoApp.css";
+
+const moreLess = (e, app, more, setMore) => {
+  setMore(!more);
+  if (more) {
+    e.target.parentElement.childNodes[0].innerText = app.description;
+  } else {
+    e.target.parentElement.childNodes[0].innerText =
+      e.target.parentElement.childNodes[0].innerText.substring(0, 256);
+  }
+};
+
 function AppInfo({ setApp, app }) {
-  // if (!appId)
-  //   fetch("https://play-store2.p.rapidapi.com/app/" + appId, {
-  //     method: "GET",
-  //     headers: {
-  //       "x-rapidapi-host": "play-store2.p.rapidapi.com",
-  //       "x-rapidapi-key": "23751b834cmsh7c2bed1e66012b8p117a95jsn54ce804c761e",
-  //     },
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setApp(data);
-  //       console.log(data);
-  //     });
+  const [revs, setRevs] = useState();
+  const [more, setMore] = useState(false);
+  useEffect(() => {
+    if (app)
+      fetch(
+        "https://app-stores.p.rapidapi.com/reviews?store=google&id=" +
+          app.id +
+          "&language=pl",
+        {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "app-stores.p.rapidapi.com",
+            "x-rapidapi-key":
+              "23751b834cmsh7c2bed1e66012b8p117a95jsn54ce804c761e",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setRevs(data);
+          console.log(data);
+        });
+  }, [app]);
 
   return app ? (
     <div className="appInfo">
@@ -47,13 +68,47 @@ function AppInfo({ setApp, app }) {
           </button>
         </div>
 
+        <div className="line"></div>
+
         <div className="screens">
           {app.screenshots.map((e) => (
             <img key={e} src={e} alt="new" />
           ))}
         </div>
 
-        <p className="desc">{app.description}</p>
+        <div className="description">
+          <h1>Opis</h1>
+          <p className="desc">{app.description.substring(0, 256)}</p>
+          <button onClick={(e) => moreLess(e, app, more, setMore)}>
+            WIĘCEIJ INFORMACJI
+          </button>
+        </div>
+
+        <div className="line"></div>
+
+        <div className="rev">
+          <h1>Opinie</h1>
+          {revs ? (
+            revs.map((e) => (
+              <div className="review">
+                <div className="centerRev">
+                  <img src={e.author.image} alt="img" />
+                  <h3>{e.rating}/5</h3>
+                </div>
+
+                <div>
+                  <h1>{e.author.name}</h1>
+                  <h2>{e.date.substring(0, 10)}</h2>
+
+                  <p>{e.content.body}</p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>Brak</p>
+          )}
+          ;
+        </div>
       </div>
     </div>
   ) : (
